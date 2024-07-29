11805a86661b6ab78a38e5f3c73dded3d2ede118
Ytikare <ytikare2@tutanota.com>
2024-07-29 16:46:16 +0300

    Create computer choice function

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function init() {
-    console.log('loaded');
+    getComputerChoice();
+}
+
+let computerChoice = 'a';
+
+function getComputerChoice() {
+    let random = (Math.random() * 10).toFixed(0);
+
+    console.log(random)
+    let result = random % 3;
+
+    switch (result) {
+        case 0:
+            computerChoice = 'rock';
+            break;
+        case 1:
+            computerChoice = 'paper';
+            break;
+        case 2:
+            computerChoice = 'scissors';
+            break;
+    }
+    
 }
