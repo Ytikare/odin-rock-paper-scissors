91ace7fd71f61a445e1f8bddf74f7f145baf25cd
Ytikare <ytikare2@tutanota.com>
2024-08-05 14:32:50 +0300

    Add game functionality

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
 const playerChoicesList = document.querySelector('.player-choices');
+const resultsDiv = document.querySelector('.result');
+const resetGameButton = document.querySelector('.reset-game');
+const scoreboardPara = document.querySelector('.scoreboard');
+const announcerMessagePara = document.querySelector('.announcer');
+
+const playerChoiceSpan = document.querySelector('.player > .choice');
+const computerChoiceSpan = document.querySelector('.computer > .choice');
+
+
+let playerWinsCount = 0;
+let computerWinsCount = 0;
+
 
 function playRound(playerChoice, computerChoice) {
+
+    playerChoiceSpan.textContent = playerChoice;
+    computerChoiceSpan.textContent = computerChoice;
+
+    let roundResult = '';
     if (computerChoice === playerChoice) {
-        console.log('Tie');
+        roundResult = `Tie!`;
     } else {
 
         if ((playerChoice === 'paper' && computerChoice === 'rock')
             || (playerChoice === 'rock' && computerChoice === 'scissors')
             || (playerChoice === 'scissors' && computerChoice === 'paper')) {
-            playerWins();
+
+            playerWinsCount++;
+            roundResult = `Player wins the round!`;
+
         } else if ((computerChoice === 'paper' && playerChoice === 'rock')
             || (computerChoice === 'rock' && playerChoice === 'scissors')
             || (computerChoice === 'scissors' && playerChoice === 'paper')) {
-            computerWins();
+
+            computerWinsCount++;
+            roundResult = `Computer wins the round!`;
+
         }
 
+        if (playerWinsCount == 5) {
+            roundResult = `Player won the game!`;
+        } else if (computerWinsCount == 5) {
+            roundResult = 'Computer won the game!';
+        }
     }
+
+    scoreboardPara.textContent = `${playerWinsCount}:${computerWinsCount}`
+    announcerMessagePara.textContent = message;
 }
 
-function playerWins() {
-    console.log('Player wins!')
-}
 
-function computerWins() {
-    console.log('Computer wins!')
-}
+
 
 function getComputerChoice() {
     let random = (Math.random() * 10).toFixed(0);
@@ -46,6 +72,9 @@ playerChoicesList.addEventListener('click', e => {
     let target = e.target;
 
     if (target.localName == 'button') {
+        resultsDiv.style.display = 'flex';
+        resetGameButton.style.display = 'block';
+
         let playerChoice = target.id;
 
         let computerChoice = getComputerChoice();
